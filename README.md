9be0d7dbd0bbd61e516e092d4ced250d45050d75
Amelia Goodson <ameliajanegoodson@gmail.com>
2020-03-28 20:28:08 +1100

    uploading final (mostly working) version

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # homework6
-Interactive weather app featuring dynamic content
+This application is an interactive weather app featuring dynamic content, displaying the current and forecasted weather conditions including temperature, humidity, and wind speed. The current weather also displays the UV index.
+
+The user's search history is saved via local storage to the sidebar, whereby the user can click on the previously searched city and load the weather information without having to type in again the name of their city. Upon page reload, the last searched-for city will be displayed, again via the use of local storage.
+
+Bootstrap was used, to create the columns and rows. Further work could improve the responsiveness of the containers.  
+
+# room for improvement / struggles
+- While the value of the UVIndex updates upon pressing a previously-searched city, the colour does not update correctly - only when searching for a new city
+- Responsiveness of five-day forecast and sidebar
+- styling and web design in general 
+- ideally remove the 12:00:00 component of the 5-day forecast, which represents 12pm. Assume I could probably use some sort of a splice or split function, but ran out of time and did not prioritise this
+
+# lessons learnt
+- Separate functions per single task executed: this avoids difficult when calling the function later but only wanting to execute part of the function
+- location of variables: need to do further research on impact of global vs local variables and impact e.g. can't call a variable that was created inside a function, in another function (a local variable)
+- discovered that local storage cannot accept/store an array properly! This is why the stringify function is necessary
+- calling APIs and displaying the output remains much easier (not not easy) for me than other functions and concepts that I'm still trying to understand or don't know exist e.g.  understanding the interaction of the different functions and why some remain undefined, lexical scope, global variables, parse and stringify functions, how to structure code etc.  
+
+
+# screenshot of application
+![image](https://user-images.githubusercontent.com/60428536/77751392-b6e47280-7079-11ea-8f95-5cb9c1f053a2.png)
+
+# link to Github repo and deployed site
+https://github.com/ameliagoodson/homework6
+https://ameliagoodson.github.io/homework6/
+
--- script.js
+++ script.js
@@ -9,23 +9,34 @@ var latCity;
 var longCity;
 var searches = JSON.parse(localStorage.getItem("saveNew")) || []
 
-showSearchHistory()
-
 var APIKey = "1b4512089e4c115253758abb4b509fb0";
 
-//click event
+//RELOAD LAST SAVED SEARCH ON PAGE RELOAD
+var historyString = localStorage.getItem('saveNew');
+if (historyString !== null) {
+    const history = JSON.parse(historyString);
+    console.log(history);
+    var lastCity = history.pop();
+    getForecast(lastCity)
+    getCoordinates(lastCity)
+}
+
+//CLICK SEARCH BUTTON TO DISPLAY 5-DAY FORECAST AND CALL FUNCTIONS
 $("#searchBtn").on('click', function (event) {
     //prevents the submit button from trying to submit a form when clicked
     event.preventDefault();
 
-    //display 5-day forecast below
+    //display 5-day forecast (previously hidden)
     $(".container-fluid").removeClass("hide")
 
     //grab the text from the input box
     var searchInp = $("#searchInp").val();
 
+    //call the necessary functions
     getForecast(searchInp);
+    addSearchHistory(searchInp);
     getCoordinates(searchInp);
+
 })
 
 function getForecast(searchInp) {
@@ -50,65 +61,44 @@ function getForecast(searchInp) {
             }
         );
 
-
-        console.log(filteredDays[0])
         //UPDATE CURRENT WEATHER INFORMATION 
         $(".cityName").text(searchInp)
         $(".current-icon").attr("src", "http://openweathermap.org/img/wn/" + filteredDays[0].weather[0].icon + ".png");
         $('.temp').text("Temperature: " + filteredDays[0].main.temp + "°C");
         $('.humidity').text("Humidity: " + filteredDays[0].main.humidity + "%");
         $('.windSpeed').text("Wind speed: " + filteredDays[0].wind.speed + " KMPH");
-        //$('.UVindex').text("UV index: " + )
 
 
         //FUTURE WEATHER INFORMATION IN A LOOP TO MINIMISE REPETITION 
-        var x;
-        for (var i = 0; i < filteredDays.length; i++) {
-            x = i + 1;
-            $('#day' + x + '-date').text(filteredDays[i].dt_txt);
-            $('#day' + x + '-temp').text("Temperature: " + filteredDays[i].main.temp + "°C");
-            $('#day' + x + '-hum').text("Humidity: " + filteredDays[i].main.humidity + "%");
-            $('#day' + x + '-description').text(filteredDays[i].weather[0].description);
-            $('#day' + x + '-icon').attr("src", "http://openweathermap.org/img/wn/" + filteredDays[i].weather[0].icon + ".png")
+        function getForecast5() {
+            var x;
+            for (var i = 0; i < filteredDays.length; i++) {
+                x = i + 1;
+                $('#day' + x + '-date').text(filteredDays[i].dt_txt);
+                $('#day' + x + '-temp').text("Temperature: " + filteredDays[i].main.temp + "°C");
+                $('#day' + x + '-hum').text("Humidity: " + filteredDays[i].main.humidity + "%");
+                $('#day' + x + '-description').text(filteredDays[i].weather[0].description);
+                $('#day' + x + '-icon').attr("src", "http://openweathermap.org/img/wn/" + filteredDays[i].weather[0].icon + ".png")
+
+            }
         }
-
-        //ADD PREVIOUS SEARCHES TO SIDEBAR
-        var savedSearch = $(".savedSearches");
-        savedSearch = searchInp;
-
-        searches.push(savedSearch)
-
-        //save more than one search     
-        //returns something like "['Tokyo'.'Sydney']". It is the oposite of JSON.parse()   
-        var citiesString = JSON.stringify(searches);
-        localStorage.setItem("saveNew", citiesString);
-        // $(".savedSearches").append(citiesString);
-        showSearchHistory()
-        //SAVE PREVIOUS SEARCHES TO SIDEBAR WITH LOCAL STORAGE
-        // localStorage.setItem("save", savedSearch);
-        // $(".savedSearches").append(savedSearch);
+        getForecast5()
     })
 }
-// $(".savedSearches").append(localStorage.getItem("saveNew"))
 
-function showSearchHistory() {
-    if (searches.length > 0) {
-        $(".savedSearches").empty();
-        for (var i = 0; i < searches.length; i++) {
-            var cityBtn = $("<button>");
-            cityBtn.text(searches[i]);
-            $(".savedSearches").append(cityBtn);
-        }
-    }
-    // $('.savedSearches button').on('click', function() {
-    //    console.log(this)
-    //    console.log('test ')
-    //     // getForecast()
-    // }) 
-};
+//ADD PREVIOUS SEARCHES TO SIDEBAR
+function addSearchHistory(searchInp) {
+    var savedSearch = $(".savedSearches");
+    savedSearch = searchInp;
 
+    searches.push(savedSearch)
+
+    var citiesString = JSON.stringify(searches);
+    localStorage.setItem("saveNew", citiesString);
+    showSearchHistory()
+}
+//GET GEOGRAPHIC COORDINATES FOR PURPOSE OF GETTING UV INDEX
 function getCoordinates(searchInp) {
-    //api.openweathermap.org/data/2.5/weather?q=tokyo&appid=1b4512089e4c115253758abb4b509fb0
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInp + "&appid=" + APIKey;
 
     $.ajax({
@@ -116,16 +106,13 @@ function getCoordinates(searchInp) {
         method: "GET"
     }).then(function (response) {
 
-        //GET COORDINATES 
         latCity = response.coord.lat;
         longCity = response.coord.lon;
 
-        console.log(latCity)
-        console.log(longCity)
         getUVIndex(latCity, longCity);
     });
 
-
+    //GET UV INDEX INFORMATION USING GEOGRAPHIC COORDINATES
 }
 function getUVIndex(latCity, longCity) {
     var queryURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + latCity + "&lon=" + longCity;
@@ -135,7 +122,7 @@ function getUVIndex(latCity, longCity) {
     }).then(function (response) {
         $(".UVindex").text("UV index: " + response.value)
 
-// function changeUVcol() {
+        // change UV index colour
         if (response.value < 4) {
             $(".UVindex").addClass("UVlow");
         }
@@ -144,10 +131,30 @@ function getUVIndex(latCity, longCity) {
             $(".UVindex").addClass("UVmed")
         };
 
-        if (response.value > 9) {
+        if (response.value > 8.1) {
             $(".UVindex").addClass("UVhigh")
         }
     });
-console.log(response.value)
+}
+//DISPLAY SEARCH HISTORY ON SIDEBAR AND ADD BUTTON CONTAIING THAT PREVIOUS SEARCH
+function showSearchHistory() {
+    if (searches.length > 0) {
+        $(".savedSearches").empty()
+        for (var i = 0; i < searches.length; i++) {
+            var cityBtn = $("<button>");
+            cityBtn.text(searches[i]);
+            $(".savedSearches").append(cityBtn);
+        }
 
-};
+    }
+    //Grab  city name from the this object. And send the city name as a parameter to the function getForecast
+    $('.savedSearches button').on('click', function () {
+        event.preventDefault()
+        var searchedCity = ($(this).text())
+
+        getForecast(searchedCity)
+        getCoordinates(searchedCity)
+        getUVIndex(searchedCity)
+        getForecast5(searchedCity)
+    })
+}
